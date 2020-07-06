4fc0d8c5fbd66c9374250638af341527f18f9121
paris <paris@example.com>
2020-07-06 18:13:49 +0900

    [update] visualize charging station

--- script/charge_station_visualize.py
+++ script/charge_station_visualize.py
@@ -5,24 +5,35 @@ import rospy
 import numpy as np
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import PoseStamped
-import dynamic_reconfigure.client
 
 class BebopMarker:
     def __init__(self):
         rospy.init_node("charge_station_visualize")
+        chargePos = [rospy.get_param("~charge_station/x",0.),rospy.get_param("~charge_station/y",0.)]
+        chargeR = rospy.get_param("~charge_station/r",0.)
         agentID = rospy.get_param("~agentID",1)
         # self.pubtopic = str(self.basename)+"/marker"
         self.pubtopic = "/visualization/chargeStation"
 
 
-        self.red = rospy.get_param("~red", default=1)
-        self.green = rospy.get_param("~green", default=0)
+        self.red = rospy.get_param("~red", default=0)
+        self.green = rospy.get_param("~green", default=1)
         self.blue = rospy.get_param("~blue", default=0)
         self.alpha = rospy.get_param("~alpha", default=.5)
         
         self.marker = Marker()
-        self.marker.ns = str(self.basename)
+        self.marker.ns = "charge_station"+str(agentID)
         self.marker.id = 0
+        self.marker.pose.position.x = chargePos[0];
+        self.marker.pose.position.y = chargePos[1];
+        self.marker.pose.position.z = 0;
+        self.marker.pose.orientation.x = 0;
+        self.marker.pose.orientation.y = 0;
+        self.marker.pose.orientation.z = 0;
+        self.marker.pose.orientation.w = 1;
+        self.marker.scale.x = chargeR*2
+        self.marker.scale.y = chargeR*2
+        self.marker.scale.z = .1
         self.marker.color.r = self.red
         self.marker.color.g = self.green
         self.marker.color.b = self.blue
@@ -30,49 +41,29 @@ class BebopMarker:
 
 
         self.markerpub = rospy.Publisher(self.pubtopic, Marker, queue_size=10)
-        self.posestampedsub = rospy.Subscriber("posestamped", PoseStamped, self.poseCallback)
 
-        self.clock = rospy.get_param("~clock",100)
+        self.clock = rospy.get_param("~clock",1)
         self.rate = rospy.Rate(self.clock)
 
-    def pcc_update_config_params(self, config):
-        self.collisionR = config.collisionR
 
-
-
-    def pcc_set_config_params(self):
-        config = self.pcc_dycon_client.get_configuration()
-        self.pcc_update_config_params(config)
-
-    def pcc_config_callback(self,config):
-        self.pcc_update_config_params(config)
-
-    def setBebopMarker(self, msg):
+    def publishChargeStation(self):
         
         self.marker.header.frame_id = "/world"
         self.marker.header.stamp = rospy.Time.now()
 
         self.marker.action = Marker.ADD
 
-        self.marker.pose = msg.pose
-
-
-        self.marker.scale.x = self.collisionR*2
-        self.marker.scale.y = self.collisionR*2
-        self.marker.scale.z = self.collisionR*2
 
         self.marker.lifetime = rospy.Duration()
-        self.marker.type = Marker.SPHERE
-
-
-    def poseCallback(self, msg):
-        self.setBebopMarker(msg)
+        self.marker.type = Marker.CYLINDER
         self.markerpub.publish(self.marker)
 
+
+
     def main(self):
-        self.pcc_set_config_params()
         while not rospy.is_shutdown():
-            rospy.spin()
+            self.publishChargeStation()
+
             self.rate.sleep()
 
 
